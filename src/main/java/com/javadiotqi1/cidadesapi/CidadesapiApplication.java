cbe147e791c340ad5f5a19ae10319ddca8aa8710
geovaneguimaraess <geoguimaraes20@gmail.com>
2021-12-12 17:03:51 -0300

    Estrutura do projeto

---
+++ src/main/java/com/javadiotqi1/cidadesapi/CidadesapiApplication.java
@@ -0,0 +1,13 @@
+package com.javadiotqi1.cidadesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javadiotqi1/cidadesapi/CidadesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadiotqi1.cidadesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
